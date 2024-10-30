fdf99b6d892bd2dec06ebbfe7cfd3586a44b8e89
Bonflintstone <freddy@railslove.com>
2024-10-30 15:51:49 +0100

    Change from frontend to backend filtering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,10 @@ class EventsController < ApplicationController
     # refetch once a day on the first request
     RefetchAll.call unless RefetchEvent.where(created_at: Date.today.beginning_of_day..).exists?
 
-    @events = Event.all.order(datetime: :asc)
+    selected_organizations = params[:organizations] || Event::ORGANIZATIONS
+
+    @event_filter = { organizations: selected_organizations }
+    @events = Event.where(organization: selected_organizations).order(datetime: :asc)
     @refetch_event = RefetchEvent.last
   end
 end
